9a58194e92ccf1a39d404b0208915aabbcecf70b
Kumar Ghimre <kumar485hyd@gmail.com>
2022-10-21 14:04:39 +0530

    min

---
+++ min.py
@@ -0,0 +1,9 @@
+import apache_beam as beam
+
+
+
+with beam.Pipeline() as pipeline:
+ data=   (
+              pipeline|beam.Create([1,3,4,6,77,99,0])
+               |beam.CombineGlobally(lambda x: min(x))
+              |beam.Map(print))
